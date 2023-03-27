3024a0b0fb53f0b46dc7996710396a0573ae8208
simonbeurel <simon.beurel@etu.univ-cotedazur.fr>
2023-03-27 10:16:25 +0200

    Mock and model for users

---
+++ front-end/src/mocks/user.mock.ts
@@ -0,0 +1,39 @@
+export const ListUser = [
+  {
+    isAdmin: true,
+    name: 'Simon',
+    surname: 'Beurel',
+    age: 21,
+    bestTheme: [],
+    sex: 'Male',
+    pathology: 0
+  },
+  {
+    isAdmin: false,
+    name: 'Maurois',
+    surname: 'Quentin',
+    age: 35,
+    bestTheme: [],
+    sex: 'Male',
+    pathology: 1
+  },
+  {
+    isAdmin: false,
+    name: 'Théa',
+    surname: 'Delias',
+    age: 85,
+    bestTheme: [],
+    sex: 'Female',
+    pathology: 3
+  },
+  {
+    isAdmin: false,
+    name: 'Lorenzo',
+    surname: 'Fromant',
+    age: 95,
+    bestTheme: [],
+    sex: 'Male',
+    pathology: 4
+  }
+
+]
