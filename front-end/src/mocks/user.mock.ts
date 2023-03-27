996af2359aa6fcf542d1417edcde39a689698011
simonbeurel <simon.beurel@etu.univ-cotedazur.fr>
2023-03-27 11:16:13 +0200

    Fix model and mock of user

--- front-end/src/mocks/user.mock.ts
+++ front-end/src/mocks/user.mock.ts
@@ -2,6 +2,7 @@ import {User} from "../models/user.model";
 
 export const mockUser: User[] = [
   {
+    id: '0',
     isAdmin: false,
     nom: "Dupont",
     prenom: "Jean",
@@ -10,11 +11,30 @@ export const mockUser: User[] = [
     pathology: 2
   },
   {
+    id: '1',
     isAdmin: true,
     nom: "Beurel",
     prenom: "Simon",
     age: 21,
     sex: "Male",
     pathology: 0
+  },
+  {
+    id: '2',
+    isAdmin: true,
+    nom: "Maurois",
+    prenom: "Quentin",
+    age: 85,
+    sex: "Male",
+    pathology: 1
+  },
+  {
+    id: '3',
+    isAdmin: true,
+    nom: "Thea",
+    prenom: "Delias",
+    age: 95,
+    sex: 'Female',
+    pathology: 4
   }
 ];
--- front-end/src/models/user.model.ts
+++ front-end/src/models/user.model.ts
@@ -1,4 +1,5 @@
 export interface User {
+  id: string;
   isAdmin: boolean;
   nom: string;
   prenom: string;
