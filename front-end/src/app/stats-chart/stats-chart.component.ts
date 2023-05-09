9925ebd0ae2d1267152fea074f90e9382fb1ab4e
TristanL06 <tristan.larguier@gmail.com>
2023-05-09 17:06:53 +0200

    Displaying chart for clicks good way

--- front-end/src/app/stats-chart/stats-chart.component.ts
+++ front-end/src/app/stats-chart/stats-chart.component.ts
@@ -1,62 +1,3 @@
-/*import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { UserService } from "../../service/user.service";
-import { PlayerStatsService } from 'src/service/playersStats.service';
-import { ActivatedRoute } from '@angular/router';
-
-import {
-  ChartComponent,
-  ApexAxisChartSeries,
-  ApexChart,
-  ApexXAxis,
-  ApexYAxis,
-  ApexTitleSubtitle,
-  ApexNonAxisChartSeries
-} from "ng-apexcharts";
-
-export type ChartOptions = {
-  series: ApexAxisChartSeries | ApexNonAxisChartSeries;
-  chart: ApexChart;
-  xaxis: ApexXAxis;
-  yaxis: ApexYAxis;
-  title: ApexTitleSubtitle;
-};
-
-
-import { playersStatsMock } from '../../mocks/playersStats.mock';
-import { map } from 'rxjs';
-import * as ApexCharts from 'apexcharts';
-
-interface ChartData {
-  name: string;
-  data: number[];
-}
-
-@Component({
-  selector: 'app-stats-chart',
-  templateUrl: './stats-chart.component.html',
-  styleUrls: ['./stats-chart.component.scss']
-})
-
-export class StatsChartComponent implements OnInit{
-  @Input() click_mode: boolean = true;
-  @ViewChild("chart") chart: ChartComponent | undefined;
-
-  private UService: UserService;
-
-  constructor(public userService: UserService, private route: ActivatedRoute) {
-    this.UService=userService;
-  }
-
-  ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    console.log(id)
-    this.UService.getUser(id).subscribe((user) => {
-      const currentUser = user;
-    });
-  }
-  public mode: string = "click";
-  public chartOptions!: Partial<ChartOptions>;
-}*/
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { UserService } from "../../service/user.service";
 import { PlayerStatsService } from '../../service/playersStats.service';
@@ -73,7 +14,6 @@ import {
   ApexYAxis,
   ApexMarkers,
   ApexFill,
-  ApexForecastDataPoints,
   ApexLegend
 } from "ng-apexcharts";
 
@@ -88,7 +28,6 @@ export type ChartOptions = {
   markers: ApexMarkers;
   colors: string[];
   fill: ApexFill;
-  forecastDataPoints: ApexForecastDataPoints;
   legend: ApexLegend;
 };
 
@@ -136,19 +75,17 @@ export class StatsChartComponent implements OnInit {
                 show: false
               }
             },
-            colors: ["#d4526e", "#33b2df","#14526e", "#c3b2df"],
+            colors: ["#d4526e", "#33b2df"],
             dataLabels: {
               enabled: false
             },
             fill: {
-              opacity: [0.24, 0.24, 1, 1]
-            },
-            forecastDataPoints: {
-              count: 2
+              type: 'solid',
+              opacity: [0.24, 1]
             },
             stroke: {
-              curve: "straight",
-              width: [0, 0, 2, 2]
+              curve: "smooth",
+              width: [0, 3]
             },
             legend: {
               show: true,
@@ -162,104 +99,6 @@ export class StatsChartComponent implements OnInit {
               }
             }
           }
-          /*
-          const data: ChartData[] = playerStats.stats.map((stat: PlayerStat) => {
-            const rangeAreaStat = stat.data.find((d: PlayerStatDataPoint) => d.x === 'rangeArea');
-            if (rangeAreaStat) {
-              console.log(rangeAreaStat);
-              return {
-                name: `${stat.name} ${currentUser.nom}`,
-                data: rangeAreaStat.y instanceof Array ? rangeAreaStat.y : [rangeAreaStat.y]
-              } as ChartData;
-            } else {
-              this.chartOptions = {
-                series: [
-                  {
-                    type: "line",
-                    name: "Moyenne",
-                    data: stat.data
-                  },
-                {
-                  type: "rangeArea",
-                  name: "Min-Max",
-                  data: stat.data
-                }
-                ],
-                chart: {
-                  height: 350,
-                  type: "rangeArea",
-                  animations: {
-                    speed: 500
-                  }
-                },
-                colors: ["#d4526e", "#33b2df", "#d4526e", "#33b2df"],
-                dataLabels: {
-                  enabled: false
-                },
-                fill: {
-                  opacity: [0.24, 0.24, 1, 1]
-                },
-                forecastDataPoints: {
-                  count: 2,
-                  dashArray: 4
-                },
-                stroke: {
-                  curve: "straight",
-                  width: [0, 0, 2, 2]
-                },
-                legend: {
-                  show: true,
-                  customLegendItems: ["Team B", "Team A"],
-                  inverseOrder: true
-                },
-                title: {
-                  text: "Range Area with Forecast Line (Combo)"
-                },
-                markers: {
-                  hover: {
-                    sizeOffset: 5
-                  }
-                }
-              };
-              console.log("data !!!")
-              console.log(stat.data);
-              return {
-                name: '',
-                data: []
-              } as ChartData;
-            }
-          });
-
-          /*this.chartOptions = {
-            series: data.map((d) => ({ name: d.name, data: d.data })),
-            chart: {
-              height: 350,
-              type: 'line',
-              zoom: {
-                enabled: false
-              },
-            },
-            title: {
-              text: `Player Stats for ${currentUser['nom']}`,
-              align: 'center',
-              style: {
-                fontSize: '16px',
-                fontWeight: 'normal',
-                color: '#666'
-              }
-            },
-            xaxis: {
-              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
-              title: {
-                text: 'Month'
-              }
-            },
-            yaxis: {
-              title: {
-                text: 'Average Stats'
-              }
-            }
-          };*/
         },
 
         (error: any) => {
