4485700512a8c1e5d9e319fdb0f0820d6a97c8ff
TristanL06 <tristan.larguier@gmail.com>
2023-05-09 15:43:22 +0200

    snapshot for saving before crash

--- front-end/src/app/stats-chart/stats-chart.component.ts
+++ front-end/src/app/stats-chart/stats-chart.component.ts
@@ -66,18 +66,30 @@ import {
   ChartComponent,
   ApexAxisChartSeries,
   ApexChart,
-  ApexXAxis,
-  ApexYAxis,
   ApexTitleSubtitle,
-  ApexNonAxisChartSeries
+  ApexXAxis,
+  ApexDataLabels,
+  ApexStroke,
+  ApexYAxis,
+  ApexMarkers,
+  ApexFill,
+  ApexForecastDataPoints,
+  ApexLegend
 } from "ng-apexcharts";
 
 export type ChartOptions = {
-  series: ApexAxisChartSeries | ApexNonAxisChartSeries;
+  series: ApexAxisChartSeries;
   chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  stroke: ApexStroke;
   xaxis: ApexXAxis;
   yaxis: ApexYAxis;
   title: ApexTitleSubtitle;
+  markers: ApexMarkers;
+  colors: string[];
+  fill: ApexFill;
+  forecastDataPoints: ApexForecastDataPoints;
+  legend: ApexLegend;
 };
 
 import { PlayerStat, PlayerStatDataPoint, PlayerStatsModel } from '../../models/playersStats.models';
@@ -101,66 +113,158 @@ export class StatsChartComponent implements OnInit {
   @ViewChild("chart") chart: ChartComponent | undefined;
 
   public mode: string = "click";
-  public chartOptions!: Partial<ChartOptions>;
+  public chartOptions: Partial<ChartOptions> | undefined;
 
-  constructor(private userService: UserService, private playerStatsService: PlayerStatsService, private route: ActivatedRoute) {}
+  constructor(private userService: UserService, private playerStatsService: PlayerStatsService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.userService.getUser(id).subscribe((user) => {
       const currentUser = user;
       PlayerStatsService.getPlayerStats(id).subscribe(
-        (playerStats: PlayerStatsModel) => {      
-        const data: ChartData[] = playerStats.stats.map((stat: PlayerStat) => {
-          const rangeAreaStat = stat.data.find((d: PlayerStatDataPoint) => d.x === 'rangeArea');
-          if (rangeAreaStat) {
-            return {
-              name: `${stat.name} ${currentUser.nom}`,
-              data: rangeAreaStat.y instanceof Array ? rangeAreaStat.y : [rangeAreaStat.y]
-            } as ChartData;
-          } else {
-            return {
-              name: '',
-              data: []
-            } as ChartData;
-          }
-        });
-        
-        this.chartOptions = {
-          series: data.map((d) => ({ name: d.name, data: d.data })),
-          chart: {
-            height: 350,
-            type: 'line',
-            zoom: {
+        (playerStats: PlayerStatsModel) => {
+          console.log(playerStats.stats)
+          this.chartOptions = {
+            series: playerStats.stats,
+            chart: {
+              height: 350,
+              type: "rangeArea",
+              animations: {
+                speed: 500
+              }
+            },
+            colors: ["#d4526e", "#33b2df", "#d4526e", "#33b2df"],
+            dataLabels: {
               enabled: false
             },
-          },
-          title: {
-            text: `Player Stats for ${currentUser['nom']}`,
-            align: 'center',
-            style: {
-              fontSize: '16px',
-              fontWeight: 'normal',
-              color: '#666'
-            }
-          },
-          xaxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
+            fill: {
+              opacity: [0.24, 0.24, 1, 1]
+            },
+            forecastDataPoints: {
+              count: 2,
+              dashArray: 4
+            },
+            stroke: {
+              curve: "straight",
+              width: [0, 0, 2, 2]
+            },
+            legend: {
+              show: false,
+              customLegendItems: ["Moyenne", "Min-Max"],
+              inverseOrder: true
+            },
             title: {
-              text: 'Month'
-            }
-          },
-          yaxis: {
-            title: {
-              text: 'Average Stats'
+              text: ""
+            },
+            markers: {
+              hover: {
+                sizeOffset: 5
+              }
             }
           }
-        };
-      },
-      
-      (error: any) => {
-        console.log(error);
-      }
+          /*
+          const data: ChartData[] = playerStats.stats.map((stat: PlayerStat) => {
+            const rangeAreaStat = stat.data.find((d: PlayerStatDataPoint) => d.x === 'rangeArea');
+            if (rangeAreaStat) {
+              console.log(rangeAreaStat);
+              return {
+                name: `${stat.name} ${currentUser.nom}`,
+                data: rangeAreaStat.y instanceof Array ? rangeAreaStat.y : [rangeAreaStat.y]
+              } as ChartData;
+            } else {
+              this.chartOptions = {
+                series: [
+                  {
+                    type: "line",
+                    name: "Moyenne",
+                    data: stat.data
+                  },
+                {
+                  type: "rangeArea",
+                  name: "Min-Max",
+                  data: stat.data
+                }
+                ],
+                chart: {
+                  height: 350,
+                  type: "rangeArea",
+                  animations: {
+                    speed: 500
+                  }
+                },
+                colors: ["#d4526e", "#33b2df", "#d4526e", "#33b2df"],
+                dataLabels: {
+                  enabled: false
+                },
+                fill: {
+                  opacity: [0.24, 0.24, 1, 1]
+                },
+                forecastDataPoints: {
+                  count: 2,
+                  dashArray: 4
+                },
+                stroke: {
+                  curve: "straight",
+                  width: [0, 0, 2, 2]
+                },
+                legend: {
+                  show: true,
+                  customLegendItems: ["Team B", "Team A"],
+                  inverseOrder: true
+                },
+                title: {
+                  text: "Range Area with Forecast Line (Combo)"
+                },
+                markers: {
+                  hover: {
+                    sizeOffset: 5
+                  }
+                }
+              };
+              console.log("data !!!")
+              console.log(stat.data);
+              return {
+                name: '',
+                data: []
+              } as ChartData;
+            }
+          });
+
+          /*this.chartOptions = {
+            series: data.map((d) => ({ name: d.name, data: d.data })),
+            chart: {
+              height: 350,
+              type: 'line',
+              zoom: {
+                enabled: false
+              },
+            },
+            title: {
+              text: `Player Stats for ${currentUser['nom']}`,
+              align: 'center',
+              style: {
+                fontSize: '16px',
+                fontWeight: 'normal',
+                color: '#666'
+              }
+            },
+            xaxis: {
+              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
+              title: {
+                text: 'Month'
+              }
+            },
+            yaxis: {
+              title: {
+                text: 'Average Stats'
+              }
+            }
+          };*/
+        },
+
+        (error: any) => {
+          console.log(error);
+        }
       );
     });
   }
