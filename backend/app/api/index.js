cb8ad14d620af7b5bcca0cbdc5a393bdbbb3f35d
TristanL06 <tristan.larguier@gmail.com>
2023-05-20 22:15:58 +0200

    working stats api

--- backend/app/api/index.js
+++ backend/app/api/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
 const UserRouter = require('./users')
+const StatsRouter = require('./stats')
 
 const router = new Router()
 router.get('/status', (req, res) => res.status(200).json('ok'))
 router.use('/users', UserRouter)
+router.use('/stats', StatsRouter)
 
 module.exports = router
