8f5828077cb1fe3472e1d54d5d130ee75f62f719
Lorenzo Froment <lorenzo.froment@etu.unice.fr>
2023-05-22 09:05:11 +0200

    add id in model

--- front-end/src/models/question.models.ts
+++ front-end/src/models/question.models.ts
@@ -2,10 +2,13 @@ export interface Answer {
   type?: string;
   value: string;
   isCorrect: boolean;
+  questionId: string;
+  id: string;
 }
 
 export interface Question {
   id: string;
+  quizId: string;
   label: string;
   answers: Answer[];
 }
