843c1d6be8fe28ce843c6ab61584b2f6b34aff03
Quentin-Maurois <quentin.maurois@etu.univ-cotedazur.fr>
2023-05-22 09:08:40 +0200

    fix on theme backend

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -1,6 +1,8 @@
 // const YourModel = require('./your-model.model.js')
 const User = require('./user.model.js')
+const Theme =  require('./theme.model.js')
 module.exports = {
   // YourModel,
     User,
+    Theme,
 }
