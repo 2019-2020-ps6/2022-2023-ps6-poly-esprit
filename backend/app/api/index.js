51fbc0d24f560dcf212d9313cd9126315feacd38
simonbeurel <simon.beurel@etu.univ-cotedazur.fr>
2023-05-22 09:31:39 +0200

    Little modification which cause a small bug :)

--- backend/app/api/index.js
+++ backend/app/api/index.js
@@ -7,6 +7,6 @@ const router = new Router()
 
 router.get('/status', (req, res) => res.status(200).json("Simon n'a pas installer postman"))
 router.use('/quizzes', QuizzesRouter)
-
+router.use('/users', UserRouter)
 
 module.exports = router
