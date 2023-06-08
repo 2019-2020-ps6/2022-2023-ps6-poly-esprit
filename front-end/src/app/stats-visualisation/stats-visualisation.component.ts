5a2ad33533a6928808b3c681a0765f1011962d80
TristanL06 <tristan.larguier@gmail.com>
2023-06-08 18:15:00 +0200

    pending changes

--- front-end/src/app/stats-visualisation/stats-visualisation.component.ts
+++ front-end/src/app/stats-visualisation/stats-visualisation.component.ts
@@ -22,6 +22,7 @@ export class StatsVisualisationComponent implements OnInit {
   reponseItem = "reponse";
   title = 'Statistiques du patient';
   statsAvailable = false;
+  displayAnalyse = true;
 
   constructor(private userService: UserService, private route: ActivatedRoute) {
     this.userId = Number(this.route.snapshot.paramMap.get("userId"))
