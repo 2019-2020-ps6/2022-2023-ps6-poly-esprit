8be324c9f67014777dbc2b1bc0683b93842afd08
simonbeurel <simon.beurel@etu.univ-cotedazur.fr>
2023-06-10 17:58:17 +0200

    Now the bouton parameter becomes a redirection to the admin-main-page if you are on admin page

--- front-end/src/app/admin-main-page/admin-main-page.component.ts
+++ front-end/src/app/admin-main-page/admin-main-page.component.ts
@@ -36,7 +36,7 @@ export class AdminMainPageComponent implements OnInit {
   inputValueTheme = "";
   constructor(private router: Router,private route: ActivatedRoute, private userService : UserService, private themeService: ThemeService, private quizService: QuizService ,public quizCreateService: QuizService, public formBuilder: FormBuilder) {
 
-    this.admin_id = this.route.snapshot.paramMap.get('id');
+    this.admin_id = this.route.snapshot.paramMap.get('id_user');
     this.UService=userService;
     this.QCService = quizService;
     this.THService = themeService;
