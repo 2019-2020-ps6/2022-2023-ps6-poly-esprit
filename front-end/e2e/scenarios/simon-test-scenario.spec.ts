073e9c83ac1345e3d757b9afd5c51f0bd6e9d6ff
simonbeurel <simon.beurel@etu.univ-cotedazur.fr>
2023-06-12 14:11:41 +0200

    Commit for allow tristan to work on the branch

--- front-end/e2e/scenarios/simon-test-scenario.spec.ts
+++ front-end/e2e/scenarios/simon-test-scenario.spec.ts
@@ -10,9 +10,29 @@ test.describe('Test poubelle pour Simon Beurel', () => {
 
   test('Test pour vérifier une redirection sur la page admin', async ({ page }) => {
     await page.goto(testUrl);
-    //Click on the text Admin Admin and check the url
     await page.click('text=Admin Admin');
     await expect(page.url()).toBe('http://localhost:4200/admin/1684739070790');
+  });
+
+  test('Vérifier que les boutons deviennent plus gros', async ({ page }) => {
+    await page.goto(testUrl);
+    await page.click('text=Simon Beurel');
+    await page.getByText('Acteurs').click();
+    await page.click('text=Les Acteurs');
+
+    // Taille du bouton de name = "Carré (4 choix affichés)"
+    const bouton = await page.getByRole('button', { name: 'Carré (4 choix affichés)' });
+    const tailleBeforeEdit = await bouton.boundingBox();
+
+    await page.getByTestId('parameter').click();
+    await page.getByTestId('big_buttons').click();
+    await page.click('text=Retour');
+
+    const bouton2 = page.getByRole('button', { name: 'Carré (4 choix affichés)' });
+    const tailleAfterEdit = await bouton2.boundingBox();
+
+    // @ts-ignore
+    expect(tailleBeforeEdit.height < tailleAfterEdit.height);
   });
 
 
