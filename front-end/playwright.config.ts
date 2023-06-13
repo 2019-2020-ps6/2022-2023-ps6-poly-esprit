b64705c2f07188b54db926cf34bd7ca96e5c5fbb
TristanL06 <tristan.larguier@gmail.com>
2023-06-13 14:14:52 +0200

    empty stats with fixtures working test

--- front-end/playwright.config.ts
+++ front-end/playwright.config.ts
@@ -3,11 +3,12 @@ import { PlaywrightTestConfig} from "@playwright/test";
 const config: PlaywrightTestConfig = {
   reporter: [['html', { open: 'always'}]],
   use: {
-    headless: true,
+    headless: false,
     viewport: { width: 1280, height: 720},
     ignoreHTTPSErrors: true,
-    video: 'on-first-retry',
-    screenshot: 'only-on-failure',
+    video: 'on',//'on-first-retry',
+    screenshot: 'on',//'only-on-failure',
+
     launchOptions: {
       slowMo: 1000,
     }
--- front-end/src/app/stats-visualisation/stats-visualisation.fixture.ts
+++ front-end/src/app/stats-visualisation/stats-visualisation.fixture.ts
@@ -2,14 +2,10 @@ import { E2EComponentFixture } from "../../../e2e/e2e-component.fixture";
 
 
 export class statsFixture extends E2EComponentFixture {
-
   async goToStats(userName: string) {
     await this.page.goto("http://localhost:4200");
     await this.page.click('text=Admin Admin');
     await this.page.click('text=Afficher les patients');
-    
-    await this.page.click('text=' + userName);
-    await this.page.click('text=Afficher les statistiques');
-
+    await this.page.locator(`.${userName}-stats-btn`).click();
   }
 }
