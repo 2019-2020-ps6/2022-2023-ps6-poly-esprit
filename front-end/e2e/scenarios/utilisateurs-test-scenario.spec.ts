b9abc07a4a188f2c4184a0416b06b0856b44b441
Quentin-Maurois <quentin.maurois@etu.univ-cotedazur.fr>
2023-06-13 15:57:52 +0200

    creation modification suppression utilisateurs

--- front-end/e2e/scenarios/utilisateurs-test-scenario.spec.ts
+++ front-end/e2e/scenarios/utilisateurs-test-scenario.spec.ts
@@ -1,29 +1,60 @@
 import { test, expect } from '@playwright/test';
 import { testUrl } from 'e2e/e2e.config';
 import {adminManagementUsersFixture} from "../../src/app/admin-management-users/admin-management-users.fixture";
+import {UserListDisplayFixture} from "../../src/app/user-list-display/user-list-display.fixture";
 
 // Scénario de test du nom du titre de la page
 
 test.describe('Tests utilisateurs', () => {
-  test('Test Creation Suppression Utilisateurs', async ({ page}) => {
+  test('Test Creation Utilisateurs', async ({ page}) => {
     await page.goto(testUrl);
 
-    const fixture = new adminManagementUsersFixture(page);
-    await fixture.addUser("Bowser", "Mario", "25", "Homme", "2", false, false, false, "");
+    const managementUsersFixture = new adminManagementUsersFixture(page);
+
+    await managementUsersFixture.gotoAjouterUtilisateur("Admin");
+    await managementUsersFixture.addUser("Bowser", "Mario", "25", "Homme", "2", false, false, false, "");
 
     await page.click('text=retour');
 
     let nomUtilisateur = await page.getByRole('heading', {name:"Bowser"});
     expect(nomUtilisateur).toBeVisible();
+  });
 
-    await fixture.modifyUser("Bowser", "Peach", "Mario", "25", "Homme", "0", false, false, false, "");
 
-    await page.click('text=Retour');
+  test('Test Modification Utilisateurs', async ({ page }) => {
+    await page.goto(testUrl);
+
+    const managementUsersFixture = new adminManagementUsersFixture(page);
+
+    await managementUsersFixture.gotoListePatients("Admin");
+
+    let nomUtilisateur = await page.getByRole('heading', {name:"Bowser"});
+    expect(nomUtilisateur).toBeVisible();
+
+    await managementUsersFixture.modifyUser("Bowser", "Peach", "Mario", "25", "Homme", "0", false, false, false, "");
+
     expect(nomUtilisateur).not.toBeVisible();
     nomUtilisateur = await page.getByRole('heading', {name:"Peach"});
+    expect(nomUtilisateur).toBeVisible();
 
-    await fixture.delUser("Peach");
+    await page.locator('.logout').click();
+
+    nomUtilisateur = await page.getByRole('heading', {name:"Peach"});
+    expect(nomUtilisateur).toBeVisible();
+  });
+
+  test('Test Suppression Utilisateurs', async ({ page }) => {
+    await page.goto(testUrl);
+
+    const managementUsersFixture = new adminManagementUsersFixture(page);
+
+    await managementUsersFixture.gotoListePatients("Admin");
+
+    let nomUtilisateur = await page.getByRole('heading', {name:"Peach"});
+
+    await managementUsersFixture.delUser("Peach");
 
     expect(nomUtilisateur).not.toBeVisible();
+
   })
 })
