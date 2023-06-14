b96d5a60ed1ed231858550fd49fe55fc0c4376df
Quentin-Maurois <quentin.maurois@etu.univ-cotedazur.fr>
2023-06-14 17:12:41 +0200

    container tests

--- front-end/playwright.config.ts
+++ front-end/playwright.config.ts
@@ -3,7 +3,7 @@ import { PlaywrightTestConfig} from "@playwright/test";
 const config: PlaywrightTestConfig = {
   reporter: [['html', { open: 'always'}]],//'only-on-failure'}]],//'always'
   use: {
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720},
     ignoreHTTPSErrors: true,
     video: 'on',//'on-first-retry',
